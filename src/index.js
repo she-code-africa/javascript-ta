6c5ded9d5f0cb96c3d786aaea85ac848aed09693
kibbumJoy <kibbumtjoy@gmail.com>
2022-06-08 13:31:02 +0100

    completed tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 // complete the function
 function prime (num) {
   // code goes here
-  if(num % 1 == 0) {
-    return true
-  }
-  else{
-    return false
-  }
+  return num % 1 == 0 ? true : false
 }
 
 function solution (arg) {
