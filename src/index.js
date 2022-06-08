a391e191f8b8bd812d08cacacaf342cb7b9d3e83
Marlyn_Mayienga <marlymayienga@gmail.com>
2022-06-08 16:42:57 +0300

    solution for function to check if number is prime

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 // complete the function
-function prime (num) {
+function prime(num) {
   // code goes here
+  if (num <= 1) return false; 
+  if (num % 2 == 0 && num > 2) return false; 
+  const s = Math.sqrt(num); // 
+  for (let i = 3; i <= s; i += 2) {
+    
+    if (num % i === 0) return false; 
+  }
+  return true;
 }
 
-function solution (arg) {
+function solution(arg) {
   return prime(arg);
 }
 
